05013cff501be12c551f9d1ab574d35e66b8e607
Iza Perenc <izarenc@onet.eu>
2016-08-20 17:53:37 +0200

    wykres!

--- ProjektIPM_JS/page2/page2.js
+++ ProjektIPM_JS/page2/page2.js
@@ -101,150 +101,35 @@ function InitChart() {
 
 
 
-WinJS.Namespace.define("Charts", {
-    Draw_RadarChart: function () {
-        var radarChartData = {
-            labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Partying", "Running"],
-            datasets: [
-                {
-                    fillColor: "rgba(220,220,220,0.5)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    data: [65, 59, 90, 81, 56, 55, 40]
-                },
-                {
-                    fillColor: "rgba(151,187,205,0.5)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    data: [28, 48, 40, 19, 96, 27, 100]
-                }
-            ]
+function drawChart() {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
 
-        }
+        title: {
+            text: "Earthquakes - per month"
+        },
+        data: [
+       {
+           type: "line",
 
-        var myRadar = new Chart(document.getElementById("RadarCanvas").getContext("2d")).Radar(radarChartData, { scaleShowLabels: false, pointLabelFontSize: 10 });
-    }
-   , Draw_BarChart: function () {
-
-       var barChartData = {
-           labels: ["January", "February", "March", "April", "May", "June", "July"],
-           datasets: [
-               {
-                   fillColor: "rgba(220,220,220,0.5)",
-                   strokeColor: "rgba(220,220,220,1)",
-                   data: [65, 59, 90, 81, 56, 55, 40]
-               },
-               {
-                   fillColor: "rgba(151,187,205,0.5)",
-                   strokeColor: "rgba(151,187,205,1)",
-                   data: [28, 48, 40, 19, 96, 27, 100]
-               }
+           dataPoints: [
+           { x: new Date(2012, 00, 1), y: 450 },
+           { x: new Date(2012, 01, 1), y: 414 },
+           { x: new Date(2012, 02, 1), y: 520 },
+           { x: new Date(2012, 03, 1), y: 460 },
+           { x: new Date(2012, 04, 1), y: 450 },
+           { x: new Date(2012, 05, 1), y: 500 },
+           { x: new Date(2012, 06, 1), y: 480 },
+           { x: new Date(2012, 07, 1), y: 480 },
+           { x: new Date(2012, 08, 1), y: 410 },
+           { x: new Date(2012, 09, 1), y: 500 },
+           { x: new Date(2012, 10, 1), y: 480 },
+           { x: new Date(2012, 11, 1), y: 510 }
            ]
-
        }
+        ]
+    });
 
-       var myLine = new Chart(document.getElementById("BarCanvas").getContext("2d")).Bar(barChartData);
-   }
-   , Draw_DoughnutChart: function () {
-       var doughnutData = [
-                {
-                    value: 30,
-                    color: "#F7464A"
-                },
-                {
-                    value: 50,
-                    color: "#46BFBD"
-                },
-                {
-                    value: 100,
-                    color: "#FDB45C"
-                },
-                {
-                    value: 40,
-                    color: "#949FB1"
-                },
-                {
-                    value: 120,
-                    color: "#4D5360"
-                }
-
-       ];
-
-       var myDoughnut = new Chart(document.getElementById("doughnutCanvas").getContext("2d")).Doughnut(doughnutData);
-   }
-   , Draw_LineChart: function () {
-       var lineChartData = {
-           labels: ["January", "February", "March", "April", "May", "June", "July"],
-           datasets: [
-               {
-                   fillColor: "rgba(220,220,220,0.5)",
-                   strokeColor: "rgba(220,220,220,1)",
-                   pointColor: "rgba(220,220,220,1)",
-                   pointStrokeColor: "#fff",
-                   data: [65, 59, 90, 81, 56, 55, 40]
-               },
-               {
-                   fillColor: "rgba(151,187,205,0.5)",
-                   strokeColor: "rgba(151,187,205,1)",
-                   pointColor: "rgba(151,187,205,1)",
-                   pointStrokeColor: "#fff",
-                   data: [28, 48, 40, 19, 96, 27, 100]
-               }
-           ]
-
-       }
-
-       var myLine = new Chart(document.getElementById("lineCanvas").getContext("2d")).Line(lineChartData);
-   }
-   , Draw_PieCanvas: function () {
-       var pieData = [
-                {
-                    value: 30,
-                    color: "#F38630"
-                },
-                {
-                    value: 50,
-                    color: "#E0E4CC"
-                },
-                {
-                    value: 100,
-                    color: "#69D2E7"
-                }
-
-       ];
-
-       var myPie = new Chart(document.getElementById("pieCanvas").getContext("2d")).Pie(pieData);
-
-   }
-   , Draw_PolarAreaCanvas: function () {
-       var chartData = [
-            {
-                value: Math.random(),
-                color: "#D97041"
-            },
-            {
-                value: Math.random(),
-                color: "#C7604C"
-            },
-            {
-                value: Math.random(),
-                color: "#21323D"
-            },
-            {
-                value: Math.random(),
-                color: "#9D9B7F"
-            },
-            {
-                value: Math.random(),
-                color: "#7D4F6D"
-            },
-            {
-                value: Math.random(),
-                color: "#584A5E"
-            }
-       ];
-       var myPolarArea = new Chart(document.getElementById("polarAreaCanvas").getContext("2d")).PolarArea(chartData);
-   }
-});
+    chart.render();
+}
+drawChart();
